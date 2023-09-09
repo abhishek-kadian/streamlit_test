4d88007210b887ea0ed1f10573c31f86c8896720
abhishek-kadian <108894096+abhishek-kadian@users.noreply.github.com>
2023-09-09 16:24:35 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
-# pip install -r requirements.txt
-
 import streamlit as st
 import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
+from docx2pdf import convert
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Image
 
 # Email configuration (replace with your own credentials)
 sender_email = "cometcreativeconsulting@gmail.com"
 sender_password = "udcoauicgjytqpld"
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
+
+# Function to create a PDF with the welcome message and image
+def create_welcome_pdf(name):
+    pdf_filename = f"Welcome_Letter_{name}.pdf"
+    
+    # Create a PDF with the message and image
+    doc = SimpleDocTemplate(pdf_filename, pagesize=letter)
+    
+    # Add the image
+    img = Image("img1.png", width=400, height=400)
+    
+    # Create content
+    content = []
+    content.append(img)
+    content.append(MIMEText(f"Hi {name}!\n\nWelcome to the team!\n\nHope you have a good time!"))
+    
+    doc.build(content)
+    
+    return pdf_filename
 
 def send_email(receiver_name, receiver_email):
     try:
@@ -21,12 +41,25 @@ def send_email(receiver_name, receiver_email):
         msg["To"] = receiver_email
         msg["Subject"] = "Welcome to the Team"
 
+        # Convert the Word document to PDF
+        convert("Letter Template.docx")
+
+        # Attach the welcome PDF
+        pdf_filename = create_welcome_pdf(receiver_name)
+        pdf_attachment = MIMEApplication(open(pdf_filename, "rb").read())
+        pdf_attachment.add_header("Content-Disposition", "attachment", filename=pdf_filename)
+        msg.attach(pdf_attachment)
+
         # Connect to the SMTP server and send the email
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, receiver_email, msg.as_string())
         server.quit()
+
+        # Clean up temporary files
+        os.remove(pdf_filename)
+        os.remove("Letter Template.pdf")
 
         return True
     except Exception as e:
